300781f851aafcd435ced93a7e5851edbf55502f
larrygf02 <raulgf_02@hotmail.com>
2019-12-16 21:26:14 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import store from './store';
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>    
+      <Provider store={store}>
+        <Header/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos}></Route>
+            <Route exact path="/productos/nuevo" component={NuevoProducto}></Route>
+            <Route exact path="/productos/editar/:id" component={EditarProducto}></Route>
+          </Switch>
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
