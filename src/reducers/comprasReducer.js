714ac58243f14fbb5ea3dcefb501509ede0ea6cf
larrygf02 <raulgf_02@hotmail.com>
2020-04-18 12:42:24 -0500

    reducer compras

---
+++ src/reducers/comprasReducer.js
@@ -0,0 +1,36 @@
+import { 
+    COMENZAR_DESCARGA_COMPRAS, 
+    COMENZAR_DESCARGA_COMPRAS_EXITOSA,
+    COMENZAR_DESCARGA_COMPRAS_ERROR 
+} from "../types/comprasTypes";
+
+const initialState = {
+    compras: [],
+    error: null,
+    loading: false,
+    compra: {}
+}
+
+export default function(state=initialState, action) {
+    switch (action.type) {
+        case COMENZAR_DESCARGA_COMPRAS:
+            return {
+                ...state,
+                loading: true,
+                compra: {}
+            }
+        case COMENZAR_DESCARGA_COMPRAS_EXITOSA:
+            return {
+                ...state,
+                loading: false,
+                compras: action.payload
+            }
+        case COMENZAR_DESCARGA_COMPRAS_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: false,
+            }
+    }
+}
+
