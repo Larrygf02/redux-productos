d351859dc850145a37598be8626c068de8094cae
larrygf02 <raulgf_02@hotmail.com>
2020-04-18 21:18:10 -0500

    Form Compra

--- src/components/NuevaCompra.js
+++ src/components/NuevaCompra.js
@@ -6,18 +6,28 @@ const NuevaCompra = () => {
             <div className="card-header">Nueva Compra</div>
             <div className="card-body">
                 <form>
-                    <div className="flex-row">
-                        <div className="form-group">
-                            <label htmlFor="">Persona</label>
-                            <select className="form-control">
-                                <option value="">1</option>
-                            </select>
+                    <div className="row">
+                        <div className="col">
+                            <div className="form-group">
+                                <label htmlFor="">Persona</label>
+                                <select className="form-control">
+                                    <option value="">1</option>
+                                </select>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="">Producto</label>
-                            <select className="form-control">
-                                <option value=""></option>
-                            </select>
+                        <div className="col">
+                            <div className="form-group ml-4">
+                                <label htmlFor="">Producto</label>
+                                <select className="form-control">
+                                    <option value=""></option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="form-group ml-4">
+                                <label htmlFor="">Cantidad</label>
+                                <input type="number" className="form-control"/>
+                            </div>
                         </div>
                     </div>
                     <button className="btn btn-danger nuevo-post d-block d-md-inline-block">
